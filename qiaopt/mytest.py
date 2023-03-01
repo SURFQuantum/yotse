02a038769e6f386ac556587aefa709a8aadac784
Ariana Torres <ariana.torres@surf.nl>
2023-03-01 15:06:00 +0100

    New test function

--- qiaopt/mytest.py
+++ qiaopt/mytest.py
@@ -1,4 +1,16 @@
 import sys
+import numpy as np 
 
-print("hola")
-print(sys.argv[1])
+def costfunction(x,y, var):
+    f = x**2 + y**2 + var*np.sin(x)
+    return f
+
+
+if __name__ == "__main__":
+    var = float(sys.argv[1])
+    print("f x y")
+    xvalues = np.linspace(0,1, 10)
+    yvalues = np.linspace(0,1, 10)
+    for x in xvalues:
+        for y in yvalues:
+            print(costfunction(x, y, var), x,y)
