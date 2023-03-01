3e9b5df8c7a4b89cbce9954550e309f5db62bd87
Ariana Torres <ariana.torres@surf.nl>
2023-03-01 15:10:28 +0100

    Refactor run

--- qiaopt/run.py
+++ qiaopt/run.py
@@ -1,34 +1,23 @@
-from qcg.pilotjob.api.manager import LocalManager
-from qcg.pilotjob.api.job import Jobs
+#!/usr/bin/env
+from tools import collectresults
+from tools import optimization
+from tools import createpoints
+from tools import submit
 
-def createpoints(parameters):
-	return [3,4,5]
-
-def submit(points, general):
-	manager = LocalManager()
-	# Loop over runs (points)
-	jobs = Jobs()
-	for i, item in enumerate(points):
-		print(i)
-		names = []
-		jobs.add(name = 'test_' + str(i), exec = 'python3',args = [general[0], item], stdout="out"+str(i))
-	print(jobs)
-	job_ids = manager.submit(jobs)
-	print(job_ids)
-	manager.wait4(job_ids)
-	manager.cleanup()
-	manager.finish()
-
-
-def main(parameters, general):
+def run(parameters, general):
 	points = createpoints(parameters)
 	submit(points, general)
-#	data = collectresults()
-#	newpoints = optimization(data)
-#	submit(newpoints, general)
+	data = collectresults()
 
+	optsteps = 3
+	for step in range(0,optsteps):
+		print(step)
+		newpoints = optimization(data)
+		submit(newpoints, general)
+		data = collectresults()
+	
 
 if __name__ == "__main__":
-	parameters = [0,1,2]
-	general = ['mytest']
-	main(parameters, general)
+	parameters = [(0,0),(1,1),(2,2)]
+	general = ['python','mytest']
+	run(parameters, general)
