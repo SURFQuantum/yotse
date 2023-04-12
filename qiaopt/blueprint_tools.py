5a608db95e6ba2cb1069e495c5a5b806a340b273
dmark04 <dmark04@web.de>
2023-04-12 16:28:42 +0200

    updated and improved blueprint tools

--- qiaopt/blueprint_tools.py
+++ qiaopt/blueprint_tools.py
@@ -3,31 +3,50 @@ import yaml
 import shutil
 from datetime import datetime
 
+from qiaopt.pre import Experiment
 
-def setup_optimization_dir(experiment, step):
-    """Create the directory for this optimization step"""
-    output_directory = os.path.join(experiment.system_setup.source_directory.split('/src')[0], 'output')
-    if step == 0:
+
+def setup_optimization_dir(experiment, step_number, job_number):
+    """Create the directory for this optimization step.
+    The structure will be as follows (for m optimization steps and n jobs)
+    > src
+        - unified_script.py
+        - processing_function.py
+        - config.yaml
+        - baseline_params.yaml
+        - qiapt_runscript.py
+    > output
+        > experiment_name_timestamp_str
+            > step0
+                > job0
+                    - stdout0.txt
+                    - dataframe_holder.pickle (?)
+                    - baseline_params_job0.yaml
+                    - config_job0.yaml
+                ...
+                > jobn
+            ...
+            > stepm
+    """
+    output_directory = os.path.join(experiment.system_setup.source_directory, '..',
+                                    experiment.system_setup.output_directory)
+    output_directory = os.path.realpath(output_directory)                                       # clean path of '..'
+    if step_number == 0 and job_number == 0:
         # for first step create timestamped project directory
         timestamp_str = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-        name_project_dir = output_directory + '/' + experiment.name + "_" + timestamp_str + "/"
-        step_output_dir = name_project_dir + f'/step{step}/output'
-        experiment.system_setup.output_directory = step_output_dir
+        name_project_dir = os.path.join(output_directory, experiment.name + "_" + timestamp_str)
+        new_working_dir = os.path.join(name_project_dir, f'step{step_number}', f'job{job_number}')
     else:
-        step_output_dir = experiment.system_setup.output_directory.split(f'/step{step-1}')[0] + f'/step{step}/output'
+        if not os.path.basename(os.path.normpath(experiment.system_setup.working_directory)).startswith("job"):
+            raise RuntimeError("The current working directory does not start with 'job'. "
+                               "New working directory can't be set up properly.")
+        new_working_dir = os.path.join(experiment.system_setup.working_directory, '..', '..',
+                                       f'step{step_number}', f'job{job_number}')
+        new_working_dir = os.path.realpath(new_working_dir)                                     # clean path of '..'
 
-    os.makedirs(step_output_dir, exist_ok=True)
-    # todo this should be extended like set_basic_directory_structure_for_job function
-
-
-def change_to_optimization_dir(experiment):
-    """Change the current working directory to the optimization directory"""
-    os.chdir(experiment.system_setup.output_directory)
-
-
-def exit_from_working_dir(experiment):
-    """Change the current working directory back to the original working directory"""
-    os.chdir(experiment.system_setup.source_directory)
+    if not os.path.exists(new_working_dir):
+        os.makedirs(new_working_dir)
+    experiment.system_setup.working_directory = new_working_dir
 
 
 def update_yaml_params(param_list, paramfile_name):
@@ -50,16 +69,26 @@ def update_yaml_params(param_list, paramfile_name):
 def replace_include_param_file(configfile_name, paramfile_name):
     # Load the YAML config file
     with open(configfile_name, 'r') as f:
-        config = yaml.safe_load(f)
+        old_config = yaml.safe_load(f)
 
-    # Find the line with the INCLUDE keyword
-    found_include = False
-    for key, value in config.items():
-        if key == "INCLUDE":
-            found_include = True
-            # Replace the included file name with the new parameter file name
-            config[key] = f"!include {paramfile_name}"
-            break
+    # Find the line with the INCLUDE keyword recursively
+    def replace_include(config, replace_str):
+        found = False
+        if isinstance(config, dict):
+            for key, value in config.items():
+                if key == 'INCLUDE':
+                    config[key] = replace_str
+                    found = True
+                elif isinstance(value, dict):
+                    found_in_nested, config[key] = replace_include(value, replace_str)
+                    found = found or found_in_nested
+                elif isinstance(value, list):
+                    for i in range(len(value)):
+                        found_in_nested, config[key][i] = replace_include(value[i], replace_str)
+                        found = found or found_in_nested
+        return found, config
+
+    found_include, new_config = replace_include(config=old_config, replace_str=f"!include {paramfile_name}")
 
     # Check if the INCLUDE keyword was found
     if not found_include:
@@ -67,35 +96,37 @@ def replace_include_param_file(configfile_name, paramfile_name):
 
     # Save the updated YAML config file
     with open(configfile_name, 'w') as f:
-        yaml.dump(config, f, default_flow_style=False)
+        yaml.dump(new_config, f, default_flow_style=False)
 
 
-def create_separate_files_for_job(experiment, datapoint_item):
+def create_separate_files_for_job(experiment: Experiment, datapoint_item: list, step_number: int, job_number: int):
     """Create a separate parameter and matching config file for this job and get ready for execution."""
-    # this should execute after the directory for the specific job is set up
+    # this should execute after the directory for the specific job is set up by setup_optimization_dir
     # 1 - copy the original param and config file to the created dir
-    source_directory = '/path/to/source/directory'
-    working_directory = '/path/to/working/directory'
-    job_name = 'job0'
+    source_directory = experiment.system_setup.source_directory
+    working_directory = experiment.system_setup.working_directory
+    old_cmdline_args = experiment.system_setup.cmdline_arguments
+    paramfile_name = old_cmdline_args['paramfile']
+    del old_cmdline_args['paramfile']
+    configfile_name = old_cmdline_args['configfile']
+    del old_cmdline_args['configfile']
+    job_name = f'job{job_number}'
+    step_name = f'step{step_number}'
 
     # Copy paramfile_name to working_directory
-    paramfile_name = 'base_params.yaml'
     paramfile_base_name, paramfile_ext = os.path.splitext(paramfile_name)
-    paramfile_new_name = paramfile_base_name + '_' + job_name + paramfile_ext
+    paramfile_new_name = paramfile_base_name + '_' + job_name + '_' + step_name + paramfile_ext
     paramfile_source_path = os.path.join(source_directory, paramfile_name)
     paramfile_dest_path = os.path.join(working_directory, job_name, paramfile_new_name)
     shutil.copy(paramfile_source_path, paramfile_dest_path)
 
     # Copy configfile_name to working_directory
-    configfile_name = 'config_file.yaml'
     configfile_base_name, configfile_ext = os.path.splitext(configfile_name)
-    configfile_new_name = configfile_base_name + '_' + job_name + configfile_ext
+    configfile_new_name = configfile_base_name + '_' + job_name + '_' + step_name + configfile_ext
     configfile_source_path = os.path.join(source_directory, configfile_name)
     configfile_dest_path = os.path.join(working_directory, job_name, configfile_new_name)
     shutil.copy(configfile_source_path, configfile_dest_path)
 
-    paramfile_name_thisjob = paramfile_dest_path
-    configfile_name_thisjob = configfile_dest_path
     # 2 - take the data_point for the current step + the active parameters/cmdlineargs and then overwrite those
     # in the respective param file
     param_list = []
@@ -106,19 +137,18 @@ def create_separate_files_for_job(experiment, datapoint_item):
                 param_list.append((param.name, datapoint_item))
             else:
                 param_list.append((param.name, datapoint_item[p]))
-    update_yaml_params(param_list=param_list, paramfile_name=paramfile_name_thisjob)
+    update_yaml_params(param_list=param_list, paramfile_name=paramfile_dest_path)
 
     # 3 - overwrite the name of the paramfile inside the configfile with the new paramfile name
-    replace_include_param_file(configfile_name=configfile_name_thisjob, paramfile_name=paramfile_name_thisjob)
+    replace_include_param_file(configfile_name=configfile_dest_path, paramfile_name=paramfile_dest_path)
     # 4 - overwrite the old_cmdline such that it no longer contains the varied params, but instead the correct paths to
     # the new param and config files
     cmdline = [os.path.join(experiment.system_setup.source_directory, experiment.system_setup.program_name)]
-    # todo : make sure we have the correct path to the file
-    cmdline.append(configfile_name_thisjob)
+    cmdline.append(configfile_dest_path)
     cmdline.append("--paramfile")
-    cmdline.append(paramfile_name_thisjob)
+    cmdline.append(paramfile_dest_path)
     # add fixed cmdline arguments
-    for key, value in experiment.system_setup.cmdline_arguments.items():
+    for key, value in old_cmdline_args.items():
         cmdline.append(key)
         cmdline.append(str(value))
 
--- tests/test_blueprint_tools.py
+++ tests/test_blueprint_tools.py
@@ -1,70 +1,226 @@
 import os
-from datetime import datetime
+import yaml
+import shutil
+import tempfile
 import unittest
-from qiaopt.pre import Experiment, SystemSetup
-from qiaopt.blueprint_tools import setup_optimization_dir, change_to_optimization_dir, exit_from_working_dir
+from datetime import datetime
+from unittest.mock import MagicMock
 
-if os.getcwd()[-5:] == "tests":
-    DUMMY_FILE = "myfunction.py"
-else:
-    DUMMY_FILE = "tests/myfunction.py"
+from qiaopt.blueprint_tools import setup_optimization_dir, update_yaml_params, replace_include_param_file, \
+    create_separate_files_for_job
 
 
-class TestOptimizationDirs(unittest.TestCase):
+class TestSetupOptimizationDir(unittest.TestCase):
     def setUp(self):
-        if not os.path.exists('src'):
-            os.mkdir('src')
-        self.experiment_name = 'test_experiment'
-        self.system_setup = SystemSetup(source_directory=os.getcwd() + '/src', program_name=DUMMY_FILE)
-        self.parameters = []
-        self.opt_info_list = []
-        self.experiment = Experiment(self.experiment_name, self.system_setup, self.parameters, self.opt_info_list)
-        self.cwd = os.getcwd()
-        self.date_time = datetime.now()
+        self.tmp_dir = tempfile.mkdtemp()
+
+    def set_mock_experiment(self):
+        # Create a mock experiment object
+        experiment = MagicMock()
+        experiment.system_setup.source_directory = os.path.join(self.tmp_dir, 'src')
+        if not os.path.exists(experiment.system_setup.source_directory):
+            os.makedirs(experiment.system_setup.source_directory)
+        experiment.system_setup.output_directory = 'output'
+        experiment.name = 'blueprint_experiment'
+        return experiment
 
     def tearDown(self):
-        os.chdir(self.cwd)
-        os.rmdir('src')
+        shutil.rmtree(self.tmp_dir)
 
-    def return_output_dir(self, step):
-        return os.path.join(os.getcwd(), 'output',
-                            f'{self.experiment_name}_{self.date_time.strftime("%Y-%m-%d_%H:%M:%S")}',
-                            f'step{step}', 'output')
+    def test_multiple_step_and_job(self):
+        experiment = self.set_mock_experiment()
+        timestamp_str = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
 
-    def test_setup_optimization_dir(self):
-        # first step
-        output_dir0 = self.return_output_dir(step=0)
-        self.assertFalse(os.path.exists(output_dir0))
-        setup_optimization_dir(self.experiment, step=0)
-        self.assertTrue(os.path.exists(output_dir0))
-        print("Output directory was:", output_dir0)
-        # subsequent step
-        output_dir1 = self.return_output_dir(step=1)
-        self.assertFalse(os.path.exists(output_dir1))
-        setup_optimization_dir(self.experiment, step=1)
-        self.assertTrue(os.path.exists(output_dir1))
-        print("Output directory was:", output_dir1)
-        os.removedirs(output_dir0)
-        os.removedirs(output_dir1)
+        dir_structure_before_call = [
+            'src',
+        ]
+        self.assertEqual(os.listdir(self.tmp_dir), dir_structure_before_call)
 
-    def test_change_to_optimization_dir(self):
-        output_dir5 = self.return_output_dir(step=5)
-        os.makedirs(output_dir5, exist_ok=True)
-        self.experiment.system_setup.output_directory = output_dir5
-        change_to_optimization_dir(self.experiment)
-        self.assertEqual(os.getcwd(), self.experiment.system_setup.output_directory)
-        print('Output directory was:', output_dir5)
-        os.removedirs(output_dir5)
+        # Call the function
+        setup_optimization_dir(experiment, 0, 0)
 
-    def test_exit_from_working_dir(self):
-        output_dir42 = self.return_output_dir(step=42)
-        os.makedirs(output_dir42, exist_ok=True)
-        self.experiment.system_setup.output_directory = output_dir42
-        change_to_optimization_dir(self.experiment)
-        exit_from_working_dir(self.experiment)
-        self.assertEqual(os.getcwd(), self.experiment.system_setup.source_directory)
-        os.removedirs(output_dir42)
-        print("ending in directory:", os.getcwd())
+        # Check that the directory structure was created correctly
+        expected_dir_structure = [
+            'src',
+            f'output/{experiment.name}_{timestamp_str}/step0/job0',
+        ]
+
+        for directory in expected_dir_structure:
+            self.assertTrue(os.path.exists(os.path.join(self.tmp_dir, directory)))
+        self.assertEqual(os.listdir(self.tmp_dir), ['output', 'src'])
+        self.assertEqual(experiment.system_setup.working_directory,
+                         os.path.join(self.tmp_dir, 'output', f'{experiment.name}_{timestamp_str}/step0/job0'))
+
+        # Call the function again
+        setup_optimization_dir(experiment, 2, 1)
+
+        # Check that the directory structure was created correctly
+        expected_dir_structure_now = [
+            'src',
+            f'output/{experiment.name}_{timestamp_str}/step0/job0',
+            f'output/{experiment.name}_{timestamp_str}/step2/job1',
+        ]
+
+        for directory in expected_dir_structure_now:
+            self.assertTrue(os.path.exists(os.path.join(self.tmp_dir, directory)))
+        self.assertEqual(os.listdir(self.tmp_dir), ['output', 'src'])
+        self.assertEqual(experiment.system_setup.working_directory,
+                         os.path.join(self.tmp_dir, 'output', f'{experiment.name}_{timestamp_str}/step2/job1'))
+
+    def test_existing_non_job_directory(self):
+        experiment = self.set_mock_experiment()
+        timestamp_str = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+        experiment.system_setup.working_directory = os.path.join(self.tmp_dir, 'output',
+                                                                 f'{experiment.name}_{timestamp_str}/step0/')
+
+        # Call the function and expect it to raise a RuntimeError
+        with self.assertRaises(RuntimeError):
+            setup_optimization_dir(experiment, 1, 1)
+
+
+class TestUpdateYamlFiles(unittest.TestCase):
+    def setUp(self):
+        self.tmp_dir = tempfile.mkdtemp()
+
+    def create_test_paramfile(self):
+        # Create a YAML file with initial parameters
+        self.param_file = os.path.join(self.tmp_dir, 'params.yaml')
+        with open(self.param_file, 'w') as f:
+            params = {
+                'num_repeaters': 1,
+                'det_efficiency': 1.0,
+                'visibility': 1.0,
+                'dark_count_prob': 0.0,
+                'emission_probability': 1.0
+            }
+            yaml.dump(params, f, default_flow_style=False)
+
+    def create_test_configfile(self):
+        # Create a YAML config file with an INCLUDE statement
+        self.config_file = os.path.join(self.tmp_dir, 'config.yaml')
+        with open(self.config_file, 'w') as f:
+            config = {
+                'network': 'blueprint_network',
+                'some_params': {
+                    '&some_params': {
+                        'INCLUDE': '!include nv_baseline_params.yaml'
+                    },
+                    '&node_type': {
+                        'type': 'some_node'
+                    },
+                    '&repeater': {
+                        '<<': '*node_type',
+                        'properties': {
+                            'end_node': False,
+                            'num_positions': 2,
+                            'port_names': [
+                                'A',  # classical communication A side
+                                'B',  # classical communication B side
+                                'ENT_A',  # entanglement generation A side
+                                'ENT_B',  # entanglement generation B side
+                            ],
+                            '<<': '*some_params'
+                        }
+                    }
+                }
+            }
+            yaml.dump(config, f, default_flow_style=False)
+
+    def test_update_yaml_params(self):
+        self.create_test_paramfile()
+        # Define the parameter updates
+        param_list = [
+            ['det_efficiency', 0.9],
+            ['dark_count_prob', 1e-5],
+            ['emission_probability', 0.1]
+        ]
+
+        # Update the YAML file
+        update_yaml_params(param_list, self.param_file)
+
+        # Load the updated parameters
+        with open(self.param_file, 'r') as f:
+            params = yaml.safe_load(f)
+
+        # Check if the parameters are updated correctly
+        expected_params = {
+            'num_repeaters': 1,
+            'det_efficiency': 0.9,
+            'visibility': 1.0,
+            'dark_count_prob': 1e-5,
+            'emission_probability': 0.1
+        }
+        self.assertEqual(params, expected_params)
+
+    def test_replace_include_param_file(self):
+        self.create_test_configfile()
+        new_param_file_name = 'new_custom_params.yaml'
+
+        # Call the function to replace the INCLUDE statement
+        replace_include_param_file(self.config_file, new_param_file_name)
+
+        # Load the updated YAML config file
+        with open(self.config_file, 'r') as f:
+            config = yaml.safe_load(f)
+
+        # Check if the INCLUDE statement is replaced correctly
+        expected_config = {
+            'network': 'blueprint_network',
+            'some_params': {
+                '&some_params': {
+                    'INCLUDE': f'!include {new_param_file_name}'
+                },
+                '&node_type': {
+                    'type': 'some_node'
+                },
+                '&repeater': {
+                    '<<': '*node_type',
+                    'properties': {
+                        'end_node': False,
+                        'num_positions': 2,
+                        'port_names': [
+                            'A',  # classical communication A side
+                            'B',  # classical communication B side
+                            'ENT_A',  # entanglement generation A side
+                            'ENT_B',  # entanglement generation B side
+                        ],
+                        '<<': '*some_params'
+                    }
+                }
+            }
+        }
+        self.assertEqual(config, expected_config)
+
+    def test_include_not_found(self):
+        # Create a YAML config file WITHOUT an INCLUDE statement
+        self.config_file = os.path.join(self.tmp_dir, 'config.yaml')
+        with open(self.config_file, 'w') as f:
+            config = {
+                'network': 'blueprint_network',
+                'some_params': {
+                    '&node_type': {
+                        'type': 'some_node'
+                    }
+                }
+            }
+            yaml.dump(config, f, default_flow_style=False)
+
+        new_param_file_name = 'new_custom_params.yaml'
+
+        # Call the function to replace the INCLUDE statement
+        with self.assertRaises(ValueError):
+            replace_include_param_file(self.config_file, new_param_file_name)
+
+    def tearDown(self):
+        # Remove the temporary directory and its contents
+        shutil.rmtree(self.tmp_dir)
+
+
+class TestCreateSeparateFiles(unittest.TestCase):
+    def test_create_separate_files_for_job(self):
+        is_implemented = False
+        self.assertTrue(is_implemented)
+        create_separate_files_for_job()
 
 
 if __name__ == '__main__':
