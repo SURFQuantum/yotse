98f75adaaf9ab2cf0c48ed26c2c6c0e37ffd6135
dmark04 <dmark04@web.de>
2023-11-01 15:16:41 +0100

    fixed git clone command

--- examples/blueprint_example/setup_blueprint_env.sh
+++ examples/blueprint_example/setup_blueprint_env.sh
@@ -2,15 +2,15 @@ python -m venv blueprint_venv
 source blueprint_venv/bin/activate
 
 # Clone the repository
-git https://gitlab.com/d.maier/simulation-code-for-requirements-for-a-processing-node-quantum-repeater-on-a-real-world-fiber-grid.git
+git clone https://gitlab.com/d.maier/simulation-code-for-requirements-for-a-processing-node-quantum-repeater-on-a-real-world-fiber-grid.git
 
 # Change to the repository directory
 cd simulation-code-for-requirements-for-a-processing-node-quantum-repeater-on-a-real-world-fiber-grid
 
 # Install the dependencies
 poetry install
-pwd
 cd ..
+rm -rf simulation-code-for-requirements-for-a-processing-node-quantum-repeater-on-a-real-world-fiber-grid
 
 # Exit the virtual environment
 deactivate
