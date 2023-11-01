2afc221c490747e61f536642c797bc14d0be6294
dmark04 <dmark04@web.de>
2023-11-01 16:31:41 +0100

    trying different env variables

--- examples/blueprint_example/setup_blueprint_env.sh
+++ examples/blueprint_example/setup_blueprint_env.sh
@@ -11,7 +11,11 @@ cd simulation-code-for-requirements-for-a-processing-node-quantum-repeater-on-a-
 # Install the dependencies
 which python
 python --version
-poetry config netsquid-pypi ${USERNAME} ${PASSWORD}
+
+export POETRY_HTTP_BASIC_NETSQUIDPYPI_USERNAME=${USERNAME}
+export POETRY_HTTP_BASIC_NETSQUIDPYPI_PASSWORD=${PASSWORD}
+
+
 poetry install
 pip list
 # Exit the virtual environment
