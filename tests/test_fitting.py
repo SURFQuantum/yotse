dcdb05d88fe1d47743dbccfc064c2bf9297691b6
dmark04 <dmark04@web.de>
2024-01-08 14:06:52 +0100

    fixed lint

--- tests/test_fitting.py
+++ tests/test_fitting.py
@@ -1,15 +1,16 @@
+"""Unit tests for the `yotse.optimization.fitting` package's `FuncFit` class."""
 import unittest
-from typing import List
-from typing import Tuple
-from unittest.mock import Mock
+from typing import Any
+
 import numpy as np
 
 from yotse.optimization.fitting import FuncFit
 
 
 class TestFitting(unittest.TestCase):
-
-    def dummy_func(self, x):
+    @staticmethod
+    def dummy_func(x: Any) -> Any:
+        """Dummy function."""
         # func = x*x*x - 1.0/x
         func = x * x
         func[func == -np.inf] = 0
@@ -22,13 +23,13 @@ class TestFitting(unittest.TestCase):
         ff = FuncFit()
 
         x = np.arange(-3, 3, 0.1, dtype=int)
-        y = dummy_func(x)
+        y = [self.dummy_func(x)]
 
         all_fits = ff.find_poly_fit(x, y)
         best_fit = all_fits[0][0]
 
         order = best_fit.order
-        error = all_fits[0][1]
+        # error = all_fits[0][1]
 
         # test output types
         self.assertIsInstance(order, int)
