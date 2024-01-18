7d5d77dabfec79b2e43e44304a7c594a3025e538
dmark04 <dmark04@web.de>
2024-01-18 13:44:59 +0100

    fixed param_switch example

--- examples/wobbly_function/example_wobbly_param_switching.py
+++ examples/wobbly_function/example_wobbly_param_switching.py
@@ -3,6 +3,8 @@ using the Yotse framework."""
 from examples.wobbly_function.example_wobbly_main import remove_files_after_run
 from examples.wobbly_function.example_wobbly_main import wobbly_pre
 from yotse.execution import Executor
+from yotse.pre import ConstraintDict
+from yotse.pre import OptimizationInfo
 from yotse.pre import Parameter
 
 
@@ -21,18 +23,40 @@ def main() -> None:
     param_switch_experiment.add_parameter(
         Parameter(
             name="z",
-            param_range=[1, 4],
+            param_range=[4, 5],
+            constraints=ConstraintDict(low=4, high=10),
             number_points=10,
             distribution="linear",
             parameter_active=False,
         )
     )
+    second_ga_opt = OptimizationInfo(
+        name="GA",
+        blackbox_optimization=True,
+        opt_parameters={
+            "num_generations": 5,  # number of iterations of the algorithm
+            # "num_points": 10,            # number of points per param to re-create , now determined by initial
+            "num_parents_mating": 5,
+            "mutation_probability": 0.2,
+            "refinement_factors": [0.5, 0.5],
+            "logging_level": 1,
+        },
+        is_active=False,
+    )
+    del param_switch_experiment.opt_info_list[
+        1
+    ]  # delete whitebox optimization that is still in the list
+    param_switch_experiment.opt_info_list.append(second_ga_opt)
+    num_gens_1 = param_switch_experiment.opt_info_list[0].opt_parameters[
+        "num_generations"
+    ]
+    num_gens_2 = param_switch_experiment.opt_info_list[1].opt_parameters[
+        "num_generations"
+    ]
 
     param_switch_example = Executor(experiment=param_switch_experiment)
 
-    for i in range(
-        param_switch_experiment.opt_info_list[0].opt_parameters["num_generations"]
-    ):
+    for i in range(num_gens_1):
         param_switch_example.run(step_number=i, evolutionary_point_generation=True)
 
     solution = param_switch_example.optimizer.suggest_best_solution()
@@ -41,14 +65,15 @@ def main() -> None:
     # Switching active params from param 1 & 2 to param 2 & 3
     param_switch_example.experiment.parameters[0].parameter_active = False
     param_switch_example.experiment.parameters[2].parameter_active = True
-    # create new datapoints with new active params
-    param_switch_example.experiment.create_datapoint_c_product()
+    # create new datapoints with new active params and write them to experiment
+    param_switch_example.experiment.data_points = (
+        param_switch_example.experiment.create_datapoint_c_product()
+    )
     # todo: maybe nice additional functionality could be that this is done automatically when params have changed.
 
-    for i in range(
-        param_switch_experiment.opt_info_list[0].opt_parameters["num_generations"],
-        2 * param_switch_experiment.opt_info_list[0].opt_parameters["num_generations"],
-    ):
+    param_switch_example.next_optimization()  # switching to next opt which is GA again
+
+    for i in range(num_gens_1, num_gens_1 + num_gens_2):
         param_switch_example.run(step_number=i, evolutionary_point_generation=True)
 
     solution = param_switch_example.optimizer.suggest_best_solution()
